aad199e4a0d798cc90242b9c52cc7f57c0feb3bd
Momo <40412640+momoproject@users.noreply.github.com>
2018-09-13 11:18:43 -0300

    Add files via upload

---
+++ scripts/debugger.js
@@ -0,0 +1,3 @@
+console.log('Total votes of every games: ' + total);
+console.log('Estimated money:            ' + '$ ' + total/1000);
+console.log('Quest progress:             ' + updt[0] + ' / ' + updt[1] + ' - (' + updt[0]/updt[1]*100 + '%)');
