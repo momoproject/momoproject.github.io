d2cb803f7357d5aaf2647e6cf1b20084b45c2e35
Momo <40412640+momoproject@users.noreply.github.com>
2018-10-12 23:32:35 -0300

    Delete debugger.js

--- scripts/debugger.js
+++
@@ -1,3 +0,0 @@
-console.log('Total votes of every games: ' + total);
-console.log('Estimated money:            ' + '$ ' + total/1000);
-console.log('Quest progress:             ' + updt[0] + ' / ' + updt[1] + ' - (' + updt[0]/updt[1]*100 + '%)');
